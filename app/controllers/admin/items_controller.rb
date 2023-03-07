356587e636f6f35fbbeac387b77ef221e20572a4
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-07 12:01:26 +0000

    items/html完了?

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,10 +5,17 @@ class Admin::ItemsController < ApplicationController
     end
     
     def new
+      @item = Item.new
       @genres = Genre.all
     end
     
     def create
+      @item = Item.new(item_params)
+      if @item.save
+        redirect_to admin_items_path(@item.id)
+      else
+        render :index
+      end
     end
     
     def show
@@ -16,13 +23,20 @@ class Admin::ItemsController < ApplicationController
     end
     
     def edit
+      @item = Item.find(params[:id])
     end
     
     def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+         redirect_to admin_items_path(@item.id)
+      else
+         render :edit_admin_items_path
+      end
     end
     
   private
     def item_params
-      params.require(:item).permit(:image, :name, :introduction, :price)
+      params.require(:item).permit(:image, :name, :introduction, :price,:is_active )
     end
 end
