0d017ec7676c9d563d490ba18c091590409d809f
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-11 13:38:33 +0000

    customer/html

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,7 +16,7 @@ class Admin::CustomersController < ApplicationController
       if @customer.update(customer_params)
          redirect_to admin_customer__path(@customer.id)
       else
-         render :edit_admin_customer_path
+         render :edit
       end
     end
    
