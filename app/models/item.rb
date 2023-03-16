4180acacf78e77780bcc55d89871f8603af32c4d
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-16 13:40:49 +0000

    items/index/show1

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+    
+    has_one_attached :image
+       
+   def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+     image.variant(resize_to_limit: [100, 100]).processed
+   end
+    
 end
