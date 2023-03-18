ceb70845e5d2789937d42dd1ea7dbfd979eedf94
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-18 00:45:59 +0000

    customer/mypage

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     get "/about" =>"homes#about",as: 'about'
-    get "/customers/mypage" =>"customers#mypage",as: 'show'
+    get "/customers/mypage" =>"customers#mypage",as: 'mypage'
     get "/customers/information/edit" =>"customers#information",as: 'edit'
     resources :addresses
     resources :orders, only: [:show, :index, :new, :create]
