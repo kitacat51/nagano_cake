52b1d941d61b25f92a821aa54fa21187f9cecca7
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-18 02:07:15 +0000

    カート機能１

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,7 @@
 class Public::CartItemsController < ApplicationController
     def index
+      @cart_items = CartItems.all
+      
     end
     
     def update
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
     
     has_one_attached :image
+    belong_to :cart_item
        
    def get_image
     unless image.attached?
@@ -8,6 +9,10 @@ class Item < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
      image.variant(resize_to_limit: [100, 100]).processed
+   end
+   
+   def with_tax_price
+    (price * 1.1).floor
    end
     
 end
