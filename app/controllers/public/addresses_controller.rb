e447bc76aa11c47dc6c590b49868a7f33a3429e0
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-19 12:32:28 +0000

    配送先

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -8,14 +8,36 @@ class Public::AddressesController < ApplicationController
     end
     
     def edit
+        @address = Address.find(params[:id])
     end
 
     def create
+      @address = Address.new(address_params)
+      if @address.save
+        redirect_to address_path(@item.id)
+      else
+        @addresses = Address.all
+        render :index
+      end
     end
     
     def update
+      @address = address.find(params[:id])
+      if @address.update(address_params)
+         redirect_to address_path(@address.id)
+      else
+         render :edit
+      end
     end
 
     def destroy
+        address = Address.find(params[:id])
+        address.destroy
+        redirect_to address_path
+    end
+    
+private
+    def address_params
+      params.require(:address).permit(:postal_code, :address, :name )
     end
 end
