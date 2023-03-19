667df7db341d4984e82aed9f727fa41d35d9274a
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-19 03:00:18 +0000

    admin動作確認

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,6 @@
 class CartItem < ApplicationRecord
     has_one_attached :image
-    has_many :items
+    belongs_to :item
     def subtotal
       item.with_tax_price * amount
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
 
    belongs_to :genre
    has_one_attached :image
+   has_many :cart_items
    
        
    def get_image(width, height)
