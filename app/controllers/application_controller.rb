0dc3facbbee4155e2f043ee9a3d91b3fd2cffb0e
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-25 06:43:24 +0000

    レイアウト3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,12 @@
 class ApplicationController < ActionController::Base
 
 #before_action :authenticate_customer!, except: [:top,:about]
-#  before_action :is_matching_login_customer, only: [:edit, :update]
+# before_action :is_matching_login_customer, only: [:edit, :update]
 #  before_action :configure_permitted_parameters, if: :devise_controller?
  # userではなくcustomer?
 
    def after_sign_in_path_for(resource)
      root_path
    end
-
-  def show
-   @customer = current_customer
-  end
-
+   
 end
