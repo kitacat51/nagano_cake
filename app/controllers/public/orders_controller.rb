8f18a3058fe02625d21f2aeb9cec8b4214e20a84
Kitazawa <121933368+kitacat51@users.noreply.github.com>
2023-03-27 13:01:15 +0000

    注文情報確認

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,17 +8,24 @@ class Public::OrdersController < ApplicationController
     
     def confirm
         @order = Order.new(order_params)
-        if params[:order][:address_number] == 0
+        if params[:order][:address_number] == "0"
           @order.postal_code = current_customer.postal_code
           @order.address = current_customer.address
  　        @order.name = current_customer.first_name + current_customer.last_name
-        elsif params[:order][:address_number] == 1
-          @addresses = Address.all
-        elsif params[:order][:address_number] == 2
-        
+        elsif params[:order][:address_number] == "1"
+          # @addresses = Address.all
+          @address = Address.find(params[:order][:address_id])
+          @order.postal_code = @address.postal_code
+          @order.address = @address.address
+          @order.name = @address.name
+        elsif params[:order][:address_number] == "2"
+          
         else
         
         end
+        
+        @cart_items = current_customer.cart_items
+        @total_payment = 0
     end
     
     def show
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,7 +2,8 @@ class CartItem < ApplicationRecord
     has_one_attached :image
     belongs_to :item
     belongs_to :customer
-    belongs_to :oeder
+    belongs_to :order
+    
     def subtotal
       item.with_tax_price * amount
     end
